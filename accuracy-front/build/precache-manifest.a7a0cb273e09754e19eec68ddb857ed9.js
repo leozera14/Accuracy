6fb04922cc3094a6ab79bdb9511a7ea05c6fda7c
Leonardo Faleiros <leonardo.faleiros@vsip.com.br>
2020-12-28 15:51:11 -0300

    Build

---
+++ accuracy-front/build/precache-manifest.a7a0cb273e09754e19eec68ddb857ed9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92cee334fc79210a9346e887280f63a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7cfef79e0fe1d6fb45f",
+    "url": "/static/css/main.54f59e49.chunk.css"
+  },
+  {
+    "revision": "7d3d782164b32d586f12",
+    "url": "/static/js/2.484e7c81.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.484e7c81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7cfef79e0fe1d6fb45f",
+    "url": "/static/js/main.8585c6a3.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "62e865e13ce4dd36023d6f8d6c53bb73",
+    "url": "/static/media/logo.62e865e1.svg"
+  }
+]);
